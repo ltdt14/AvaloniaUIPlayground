63c0c8f984da17473788a36eaadea60d14de6346
Lasse Todt <lassetodt@mailbox.org>
2018-07-08 21:30:52 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using AvaloniaUIPlayground.ViewModels;
+using AvaloniaUIPlayground.Views;
+
+namespace AvaloniaUIPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace AvaloniaUIPlayground.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _textBoxText = "Type in the box";
+
+        public MainWindowViewModel()
+        {
+            MyCommand = new DelegateCommand(Change);
+        }
+
+        public ICommand MyCommand { get; }
+
+        public string TextBoxText
+        {
+            get { return _textBoxText; }
+            set { this.RaiseAndSetIfChanged(ref _textBoxText, value); }
+        }
+
+        public void Change()
+        {
+            TextBoxText = "Changed text with button.";
+        }
+
+    }
+}
